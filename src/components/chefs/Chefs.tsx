e91264360a214b0bd0d8dc8adcd4585b8fcc2d54
Assafkatz99 <assafkatz99@gmail.com>
2023-02-13 19:03:20 +0200

    Chefs first commit

--- src/components/chefs/Chefs.tsx
+++ src/components/chefs/Chefs.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { White_button } from "../../typs/buttons/Styled_buttons";
+import Chef_img_card from "../homepage/hp-components/Chef_img_card/Chef_img_card";
+import Footer from "../homepage/hp-components/footer/Footer";
 import Navbar from "../navbar/Navbar";
-// import "./chefs.css";
+import "./Chefs.css";
 
 
 const Chefs: React.FC = () => {
       return (<>
         <Navbar />
-        <h1>Chefs</h1>
+
+        <div className="filters">
+          <div className="buttons">
+            <White_button>All</White_button>
+            <White_button>New</White_button>
+            <White_button>Most Viewed</White_button>
+          </div>
+        </div>
+
+        <div className="chefs_grid_div">
+        <div className="chefs_grid">
+        {
+        (() => {
+          let  restaurants = [];
+          for (let i = 0; i < 10; i++) {
+            restaurants.push(<Chef_img_card layout="chef_page" chef_f_name="Yossi" chef_l_name="issoY" chef_img_src="https://static.vecteezy.com/packs/media/vectors/term-bg-1-3d6355ab.jpg" />);
+          }
+          return restaurants;
+        })()
+      }
+        </div>
+        </div>
+      <Footer />
         </>
       )
     }
--- src/components/homepage/hp-components/Chef_of_the_week/Chef_of_the_week.tsx
+++ src/components/homepage/hp-components/Chef_of_the_week/Chef_of_the_week.tsx
@@ -10,7 +10,7 @@ const Chef_of_the_week: React.FC= () => {
       return (<div className="cotw_div">
         <h1>CHEF OF THE WEEK:</h1>
         <div className="upper_cotw_div">
-            <Chef_img_card chef_f_name="Yossi" chef_l_name="issoY" chef_img_src="https://static.vecteezy.com/packs/media/vectors/term-bg-1-3d6355ab.jpg" />
+            <Chef_img_card layout="homepage" chef_f_name="Yossi" chef_l_name="issoY" chef_img_src="https://static.vecteezy.com/packs/media/vectors/term-bg-1-3d6355ab.jpg" />
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum et ipsam inventore vitae eius similique, magnam sapiente hic culpa! Distinctio, neque saepe. Consequatur libero perferendis laboriosam vero accusamus corrupti veniam, veritatis iure fugit, blanditiis hic cupiditate rerum culpa quam quas eum odit nesciunt illo commodi. Accusantium et ab quod modi?</p>
         </div>
         <div className="botton_cotw_div">
