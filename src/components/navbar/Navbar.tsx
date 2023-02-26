0d1f3c08a022f9573cb384321761af0adb4caa2b
Assafkatz99 <assafkatz99@gmail.com>
2023-02-26 18:22:18 +0200

    start of mobile mode

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,18 +4,27 @@ import "./navbar.css";
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar">
-      <button
-        id="logo-button-id"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        <img className="logo" src="/assets/logos/logo.svg" />
-      </button>
+      <div>
+        <button
+          id="logo-button-id"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <img src="/assets/logos/small-logo.svg"></img>
+          <img src="/assets/logos/EPICURE-logo.svg"></img>
+        </button>
+      </div>
       <div className="menu">
-        <div className="menu-left">
+        <div className={`menu-left ${menuOpen ? "show" : ""}`}>
           <NavLink
             to="/restaurants"
             className={({ isActive }) => (isActive ? "underline" : "notActive")}
@@ -28,23 +37,41 @@ const Navbar: React.FC = () => {
           >
             Chefs
           </NavLink>
+          <hr className={"menu-left-mobile"}></hr>
+          <NavLink to="/" className={"menu-left-mobile"}>
+            Contact Us
+          </NavLink>
+          <NavLink to="/" className={"menu-left-mobile"}>
+            Term of Use
+          </NavLink>
+          <NavLink to="/" className={"menu-left-mobile"}>
+            Privacy Policy
+          </NavLink>
         </div>
 
         <div className="menu-right">
-          <input
-            type="text"
-            name="search"
-            placeholder="Search for restaurant cuisine, chef"
-          />
-          <button>
-            <img className="menu-icons" src="/assets/logos/person_button.svg" />
+          <button className="hamburger-button" onClick={toggleMenu}>
+            <img src="/assets/logos/hamburger-icon.svg" />
           </button>
-          <button>
-            <img
-              className="menu-icons"
-              src="/assets/logos/shopping_bag_button.svg"
+          <div className="menu-right-icons">
+            <input
+              type="text"
+              name="search"
+              placeholder="Search for restaurant cuisine, chef"
             />
-          </button>
+            <button>
+              <img
+                className="menu-icons"
+                src="/assets/logos/person_button.svg"
+              />
+            </button>
+            <button>
+              <img
+                className="menu-icons"
+                src="/assets/logos/shopping_bag_button.svg"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </div>
