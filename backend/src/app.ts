c0c9f3aaf35fc47f480dd28d7f1e26abbceb100f
Assafkatz99 <assafkatz99@gmail.com>
2023-03-12 10:56:52 +0200

    connecting db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { connectToDB } from "./connection"
 const app = express();
 app.get("/", function (req, res) {
    res.send("Hello World");
 });
 app.listen(8000, () => console.log("Listening..."));
+
+connectToDB()
